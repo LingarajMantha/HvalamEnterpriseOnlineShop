5a3d9eef51a9a64bfe40ff1e8bb160a823e0c4af
LingarajMantha <lingarajmantha10@gmail.com>
2024-10-22 00:01:41 +0530

    Adding backend springboot

---
+++ Backend_HvalamOnlineShop/src/main/java/com/example/Backend_HvalamOnlineShop/BackendHvalamOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_HvalamOnlineShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHvalamOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHvalamOnlineShopApplication.class, args);
+	}
+
+}
---
+++ Backend_HvalamOnlineShop/src/test/java/com/example/Backend_HvalamOnlineShop/BackendHvalamOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_HvalamOnlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHvalamOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
